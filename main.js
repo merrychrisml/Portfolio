111c680a96a0b17e5f518ae4d75664bfe6ae7dba
merrychrisml <80391881+merrychrisml@users.noreply.github.com>
2021-12-16 16:53:50 +0900

    Javascript : Navbar Click to Scroll

--- main.js
+++ main.js
@@ -6,8 +6,21 @@ const navbarHeight = navbar.getBoundingClientRect().height;
 
 document.addEventListener("scroll", () => {
   if (window.scrollY > navbarHeight) {
-      navbar.classList.add('navbar--dark');
+    navbar.classList.add("navbar--dark");
   } else {
-      navbar.classList.remove('navbar--dark');
+    navbar.classList.remove("navbar--dark");
   }
+});
+
+// Handle scrolling when tapping on the nav menu
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  console.log(event.target.dataset.link);
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({behavior:'smooth'});
 });
