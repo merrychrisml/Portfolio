6595962aba6f657d1c3e348d7f3a9c5647ba7daf
merrychrisml <80391881+merrychrisml@users.noreply.github.com>
2021-12-17 13:35:16 +0900

    Javascript: Navbar toggle

--- main.js
+++ main.js
@@ -20,8 +20,15 @@ navbarMenu.addEventListener("click", (event) => {
   if (link == null) {
     return;
   }
+  navbarMenu.classList.remove('open');
   scrollIntoView(link);
 });
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
+})
 
 // Handle scrolling when tapping on contact me button
 const contactMe = document.querySelector(".home__contact");
