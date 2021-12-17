6c8ba932dc85a5ddc667222f266268ad7e590805
merrychrisml <80391881+merrychrisml@users.noreply.github.com>
2021-12-17 11:51:08 +0900

    Javascript: project filtering

--- main.js
+++ main.js
@@ -37,20 +37,40 @@ document.addEventListener("scroll", () => {
 });
 
 // Show "arrow up" button when scrolling down
-const arrowUp = document.querySelector('.arrow-up')
-document.addEventListener('scroll', () => {
-    if(window.scrollY > homeHeight/2) {
-        arrowUp.classList.add('visible');
-    } else {
-        arrowUp.classList.remove('visible');
-    }
-})
+const arrowUp = document.querySelector(".arrow-up");
+document.addEventListener("scroll", () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.classList.add("visible");
+  } else {
+    arrowUp.classList.remove("visible");
+  }
+});
 
 // Handle click on the "arrow up" button
-arrowUp.addEventListener('click', () => {
-    scrollIntoView('#home');
-})
+arrowUp.addEventListener("click", () => {
+  scrollIntoView("#home");
+});
 
+// Projects
+const workBtnContainer = document.querySelector(".work__categories");
+const projectContainer = document.querySelector(".work__projects");
+const projects = document.querySelectorAll(".project");
+workBtnContainer.addEventListener("click", (e) => {
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+  projectContainer.classList.add("anim-out");
+ 
+
+  setTimeout(() => {
+    projects.forEach((project) => {
+      if (filter === "*" || filter === project.dataset.type) {
+        project.classList.remove("invisible");
+      } else {
+        project.classList.add("invisible");
+      }
+    });
+    projectContainer.classList.remove("anim-out");
+  }, 300);
+});
 
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
