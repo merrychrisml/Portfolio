c9feda502d9a7baaf1a08ee1ab954cbf2f1589e5
merrychrisml <80391881+merrychrisml@users.noreply.github.com>
2021-12-17 12:38:02 +0900

    Javascript: activate work button

--- main.js
+++ main.js
@@ -57,9 +57,14 @@ const projectContainer = document.querySelector(".work__projects");
 const projects = document.querySelectorAll(".project");
 workBtnContainer.addEventListener("click", (e) => {
   const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
-  projectContainer.classList.add("anim-out");
- 
+  // Remove Selection from the previous item and Select the new one
+  const active = document.querySelector(".category__btn.selected");
+  active.classList.remove("selected");
+  const target =
+    e.target.nodeName === "BUTTON" ? e.target : e.target.parentNode;
+  target.classList.add("selected");
 
+  projectContainer.classList.add("anim-out");
   setTimeout(() => {
     projects.forEach((project) => {
       if (filter === "*" || filter === project.dataset.type) {
